44f280ba5ff10a4f95074953fe78fb2b074471e3
Wolfram Schneider <wosch@FreeBSD.org>
2011-01-30 11:06:28 +0100

    check for tagcloud

--- bin/bbbike-build-runtime-perl.pl
+++ bin/bbbike-build-runtime-perl.pl
@@ -2,8 +2,12 @@
 # Copyright (c) 2009-2011 Wolfram Schneider, http://bbbike.org
 #
 # test if all perl modules are installed
+#
+# sudo /opt/local/bin/cpan HTML::TagCloud
+
 
 use Text::Unidecode;
+use HTML::TagCloud;
 
 1;
 
