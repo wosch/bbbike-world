28801a3432e0dda65a0085603578beaed3bc1329
Wolfram Schneider <wosch@freebsd.org>
2011-09-17 11:53:42 +0200

    indent

--- bin/bbbike-reorder-cities.pl
+++ bin/bbbike-reorder-cities.pl
@@ -22,12 +22,12 @@ my %printed;
 for ( my $i = 0 ; $i < $count ; $i++ ) {
     next if exists $printed{$i};
     print $list[$i], "\n";
-    $printed{$i}=1;
+    $printed{$i} = 1;
 
-    if ( (my $j = $i + $first) < $count) {
-    	next if exists $printed{$j}; # should never reached
-    	print $list[$j], "\n";
-    	$printed{$j}=1;
+    if ( ( my $j = $i + $first ) < $count ) {
+        next if exists $printed{$j};    # should never reached
+        print $list[$j], "\n";
+        $printed{$j} = 1;
     }
 }
 
