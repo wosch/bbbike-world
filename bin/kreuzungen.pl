fd305f9dc82de4fd569ac9de5e800564fcc13bff
Wolfram Schneider <wosch@freebsd.org>
2011-09-19 22:01:50 +0200

    kreuzungen.pl - extract list of crossings

---
+++ bin/kreuzungen.pl
@@ -0,0 +1,90 @@
+#!/usr/local/bin/perl 
+# Copyright (c) 2009-2011 Wolfram Schneider, http://bbbike.org
+#
+# kreuzungen.pl - extract list of crossings
+
+use Data::Dumper;
+use IO::File;
+
+use lib '.';
+use lib './lib';
+use Strassen;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = 0.1;
+
+use Getopt::Long;
+
+my $debug       = 1;            # 0: quiet, 1: normal, 2: verbose
+my $data_dir    = "data-osm";
+my $granularity = 10000;
+
+binmode \*STDOUT, ":utf8";
+binmode \*STDERR, ":utf8";
+
+sub usage () {
+    <<EOF;
+usage: $0 [--debug={0..2}] [options] cities
+
+--debug=0..2	  debug option
+--data-dir=/path/to/data-osm  default: $data_dir
+EOF
+}
+
+# fill wgs84 coordinate with trailing "0" if to short
+sub padding {
+    my $x   = shift;
+    my $len = length($granularity);
+
+    my $x2 = $x;
+
+    $x2 =~ s/^[\-\+]?\d+\.?//;
+
+    for ( my $i = length($x2) ; $i < $len ; $i++ ) {
+        $x .= "0";
+    }
+
+    return $x;
+}
+
+sub crossing {
+    my %args     = @_;
+    my $city     = $args{'city'};
+    my $data_dir = $args{'data_dir'};
+
+    my $s             = Strassen->new("$data_dir/$city/strassen");
+    my $all_crossings = $s->all_crossings();
+
+    print "City: $city, crosses: $#$all_crossings\n" if $debug >= 2;
+    my @data;
+    foreach my $c (@$all_crossings) {
+        push @data,
+          padding( $c->[0] ) . "," . padding( $c->[1] ) . "\t" . $c->[2] . "\n";
+    }
+
+    my $file     = "$data_dir/$city/opensearch.crossing";
+    my $file_tmp = $file . ".tmp";
+    my $fh = IO::File->new( $file_tmp, "w" ) or die "open $file_tmp: $!\n";
+
+binmode $fh, ":utf8";
+    print $fh join "", sort @data;
+
+    rename( $file_tmp, $file ) or die "rename $file: $!\n";
+}
+
+GetOptions(
+    "debug=i"    => \$debug,
+    "data-dir=s" => \$data_dir,
+) or die usage;
+
+my @cities = @ARGV;
+
+die "No cities given\n" . usage if scalar(@cities) <= 0;
+
+foreach my $city (@cities) {
+    &crossing( 'city' => $city, 'data_dir' => $data_dir );
+}
+
