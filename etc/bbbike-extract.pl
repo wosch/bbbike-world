92dca08e8719d6cbeff507b6ed7f175362ac865c
Wolfram Schneider <wosch@freebsd.org>
2012-01-14 16:51:10 +0100

    documentation

--- etc/bbbike-extract.pl
+++ etc/bbbike-extract.pl
@@ -1,4 +1,6 @@
-# config file for bbbike/world/bin/extract.pl
+# example config file for bbbike/world/bin/extract.pl
+# 
+# put it in ~/.bbbike-extract 
 
 #$option{'homepage'} = 'http://dev2.bbbike.org/osm/extract';
 
