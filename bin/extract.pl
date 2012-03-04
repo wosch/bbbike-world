93d2b83c3573417a749546106faf7918b2bd6304
Wolfram Schneider <wosch@freebsd.org>
2012-03-04 13:34:34 +0100

    debugging

--- bin/extract.pl
+++ bin/extract.pl
@@ -727,6 +727,7 @@ else {
     # Oops, are jobs are in use, give up
     die "Cannot get lock for jobs 1..", $option->{'max_jobs'}, "\n"
       if !$lockfile;
+    warn "Use lockfile $lockfile\n" if $debug;
 
     my @list = parse_jobs(
         'files' => \@files,
