dc495722fd904f58bd39c95e9026f70d54254ca6
Wolfram Schneider <wosch@freebsd.org>
2012-03-31 21:59:16 +0200

    fix max jobs warning

--- bin/extract.pl
+++ bin/extract.pl
@@ -839,7 +839,7 @@ else {
     }
 
     # Oops, are jobs are in use, give up
-    die "Cannot get lock for jobs 1..", $option->{'max_jobs'}, "\n"
+    die "Cannot get lock for jobs 1..$max_jobs\n"
       if !$lockfile;
     warn "Use lockfile $lockfile\n" if $debug;
 
