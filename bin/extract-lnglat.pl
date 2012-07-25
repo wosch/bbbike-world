f21e706645aa23cfb68af78edf16b3a00690618f
Wolfram Schneider <wosch@freebsd.org>
2012-07-25 18:53:21 +0200

    heatmap fixes, use LOG_DIR for logfiles

--- bin/extract-lnglat.pl
+++ bin/extract-lnglat.pl
@@ -7,15 +7,17 @@
 
 # Aachen:::de::5.88 50.60 6.58 50.99:294951::
 
+my $heatmap = $ENV{HEATMAP} || "heatmap";
+
 for ( -180 .. 179 ) {
     $a = $_ + 1;
 
     print
       qq[make -s -f Makefile.osm CITIES_FILE=tmp/cities_${_}.txt],
-      qq[ CITIES_DB=tmp/cities_${_}.csv],
-      qq[ OSM_DIR=tmp/osm-lnglat/$_ ],
-qq[ OSM_PLANET_PBF=tmp/osm-lng/p_${_}_-89_${a}_89/p_${_}_-89_${a}_89.osm.pbf ],
-      qq[cities-pbf > tmp/log.extract-lnglat.$_\0];
+      qq[ CITIES_DB=$heatmap/cities_${_}.csv],
+      qq[ OSM_DIR=$heatmap/osm-lnglat/$_ ],
+qq[ OSM_PLANET_PBF=$heatmap/osm-lng/p_${_}_-89_${a}_89/p_${_}_-89_${a}_89.osm.pbf ],
+      qq[cities-pbf > $heatmap/tmp/log.extract-lnglat.$_\0];
 }
 
 # ( cd tmp/osm-lnglat; find . -name '*.pbf' | xargs du -k |sort -n | perl -npe 's,\S+/p_(.*?).osm.pbf,\1,; s,_, ,g' ) > tmp/heatmap.csv
--- bin/extract-lnglat0.sh
+++ bin/extract-lnglat0.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+: ${HEATMAP_DIR="heatmap"}
+
 for i in `perl -e 'for (-180 .. 179) { print "$_ " }'`
 do
-  ./world/bin/extract-lnglat0.pl $i > tmp/cities_$i.csv
-  awk -F: '{ print $1 } ' tmp/cities_$i.csv > tmp/cities_$i.txt
+  ./world/bin/extract-lnglat0.pl $i > $HEATMAP_DIR/cities_$i.csv
+  awk -F: '{ print $1 } ' $HEATMAP_DIR/cities_$i.csv > $HEATMAP_DIR/cities_$i.txt
 done
 
