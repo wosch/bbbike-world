04d34fd5cb103d508e7c8ebde0f36afd760645b8
Wolfram Schneider <wosch@freebsd.org>
2012-07-25 19:16:53 +0200

    do not generate *.osm.gz files

--- bin/extract-lnglat.pl
+++ bin/extract-lnglat.pl
@@ -18,8 +18,6 @@ for ( -180 .. 179 ) {
       qq[ CITIES_DB=$heatmap/cities_${_}.csv],
       qq[ OSM_DIR=$heatmap/osm-lnglat/$_ ],
 qq[ OSM_PLANET_PBF=$heatmap/osm-lng/p_${_}_-89_${a}_89/p_${_}_-89_${a}_89.osm.pbf ],
-      qq[cities-pbf > $heatmap/tmp/log.extract-lnglat.$_\0];
+      qq[_cities-pbf > $heatmap/tmp/log.extract-lnglat.$_\0];
 }
-
-# ( cd tmp/osm-lnglat; find . -name '*.pbf' | xargs du -k |sort -n | perl -npe 's,\S+/p_(.*?).osm.pbf,\1,; s,_, ,g' ) > tmp/heatmap.csv
 
