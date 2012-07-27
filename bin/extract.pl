eccd664406c082202ed3e8b5ffd8ff25cd64bf97
Wolfram Schneider <wosch@freebsd.org>
2012-07-27 23:39:19 +0200

    refactor

--- bin/extract.pl
+++ bin/extract.pl
@@ -471,7 +471,7 @@ sub run_extracts {
     }
 
     warn join( " ", @data ), "\n" if $debug >= 2;
-    return (\@data, \@fixme);
+    return ( \@data, \@fixme );
 }
 
 # compute SHA2 checksum for extract file
@@ -608,10 +608,11 @@ sub send_email {
             }
         }
         elsif ( $format =~ /^garmin-(osm|cycle|leisure).zip$/ ) {
-	    my $style = $1;
+            my $style = $1;
             $file =~ s/\.pbf$/.$format/;
             if ( !cached_format($file) ) {
-                @system = ( @nice, "$dirname/pbf2osm", "--garmin-$style", $pbf_file );
+                @system =
+                  ( @nice, "$dirname/pbf2osm", "--garmin-$style", $pbf_file );
                 warn "@system\n" if $debug >= 2;
                 system(@system) == 0 or die "system @system failed: $?";
             }
@@ -741,16 +742,16 @@ EOF
 
 # prepare to sent mail about extracted area
 sub fix_pbf {
-    my $files       = shift;
+    my $files = shift;
 
     # all scripts are in these directory
     my $dirname = dirname($0);
     my $pbf2pbf = "$dirname/pbf2pbf";
 
-    my @system;    
+    my @system;
     foreach my $pbf (@$files) {
-        @system = ($pbf2pbf, $pbf);
-            system(@system) == 0
+        @system = ( $pbf2pbf, $pbf );
+        system(@system) == 0
           or die "system @system failed: $?";
     }
 }
@@ -897,12 +898,19 @@ die "Max jobs: $max_jobs out of range!\n" . &usage
 die "Max areas: $max_areas out of range!\n" . &usage
   if $max_areas < 1 || $max_areas > 30;
 
+my @files = get_jobs( $spool->{'confirmed'} );
+if ( !scalar(@files) ) {
+    print "Nothing to do\n" if $debug >= 2;
+    exit;
+}
+
 my $loadavg = &get_loadavg;
 if ( $loadavg > $option->{max_loadavg} ) {
     my $max_loadavg_jobs = $option->{max_loadavg_jobs};
     if ( $max_loadavg_jobs >= 1 ) {
         warn
-"Load avarage $loadavg is to high, reset max jobs to: $max_loadavg_jobs\n";
+"Load avarage $loadavg is to high, reset max jobs to: $max_loadavg_jobs\n"
+          if $debug >= 2;
         $max_jobs = $max_loadavg_jobs;
     }
     else {
@@ -914,13 +922,6 @@ if ( defined $timeout ) {
     die "Timeout: $timeout out of range!\n" . &usage
       if ( $timeout < 1 || $timeout > 86_400 );
     $alarm = $timeout;
-}
-
-my @files = get_jobs( $spool->{'confirmed'} );
-
-if ( !scalar(@files) ) {
-    print "Nothing to do\n" if $debug >= 2;
-    exit;
 }
 
 {
@@ -963,9 +964,10 @@ if ( !scalar(@files) ) {
 
     ###########################################################
     # main
-    my ($system, $new_pbf_files) = run_extracts( 'spool' => $spool, 'poly' => $poly );
+    my ( $system, $new_pbf_files ) =
+      run_extracts( 'spool' => $spool, 'poly' => $poly );
     my @system = @$system;
-    
+
     ###########################################################
 
     my $time      = time();
