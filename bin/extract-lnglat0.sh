9687b3bd1ee0e26fb13ef7043e227bc88ebd19f5
Wolfram Schneider <wosch@freebsd.org>
2012-08-02 18:29:13 +0200

    re-organize ./tiles directories, put everything in tiles/cities

--- bin/extract-lnglat0.sh
+++ bin/extract-lnglat0.sh
@@ -4,7 +4,7 @@
 
 for i in `perl -e 'for (-180 .. 179) { print "$_ " }'`
 do
-  ./world/bin/extract-lnglat0.pl $i > $TILES_DIR/cities_$i.csv
-  awk -F: '{ print $1 } ' $TILES_DIR/cities_$i.csv > $TILES_DIR/cities_$i.txt
+  ./world/bin/extract-lnglat0.pl $i > $TILES_DIR/cities/cities_$i.csv
+  awk -F: '{ print $1 } ' $TILES_DIR/cities/cities_$i.csv > $TILES_DIR/cities/cities_$i.txt
 done
 
