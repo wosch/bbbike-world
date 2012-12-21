266672ff22f9147eff41f23b5f43fade12d6cb76
Wolfram Schneider <wosch@freebsd.org>
2012-12-21 22:17:49 +0100

    new map: Maptookit Topo (OSM)

--- tile/web/slippymap.js
+++ tile/web/slippymap.js
@@ -122,6 +122,13 @@ function init() {
         numZoomLevels: 17
     }));
 
+    map.addLayer(new OpenLayers.Layer.OSM("Maptookit Topo (OSM)", ['http://tile1.maptoolkit.net/terrain/${z}/${x}/${y}.png', 'http://tile2.maptoolkit.net/terrain/${z}/${x}/${y}.png'], {
+        numZoomLevels: 19,
+        tileOptions: {
+            crossOriginKeyword: null
+        },
+    }));
+
     map.addLayer(new OpenLayers.Layer.OSM("Esri", "http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/${z}/${y}/${x}.png", {
         tileOptions: {
             crossOriginKeyword: null
@@ -309,7 +316,6 @@ function init() {
     bing();
 
     // http://xbb.uz/openlayers/i-Yandex.Maps
-
 
     function yandex_getTileURL(bounds) {
         var r = this.map.getResolution();
