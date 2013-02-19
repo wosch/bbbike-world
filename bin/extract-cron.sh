a1de8d78e60c7a3d86cd215c183b6438aa2ea0b8
Wolfram Schneider <wosch@freebsd.org>
2013-02-19 17:52:51 +0100

    wrapper for extract.pl script

---
+++ bin/extract-cron.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Copyright (c) 2009-2013 Wolfram Schneider, http://bbbike.org
+#
+# extract-cron.sh - wrapper for extract.pl script
+#
+
+PATH=/bin:/usr/bin; export PATH
+set -e
+
+prog=$(echo $0 | perl -npe 's/-cron\.sh$/.pl/')
+subject="extract status:"
+
+tmp=$(mktemp -t extract.XXXXXXXXXXX)
+
+$prog --debug=1 $@ > $tmp 2>&1
+error=$?
+
+# it run
+if [ -s $tmp ]; then
+    mail -s "$subject $error" $(whoami) < $tmp
+fi
+rm -f $tmp
+
