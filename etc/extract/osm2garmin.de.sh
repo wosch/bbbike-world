5701f3fa7b4f8dad03c495591ed4e85fcc2e4eff
Wolfram Schneider <wosch@freebsd.org>
2013-03-06 21:01:16 +0100

    spaces

--- etc/extract/osm2garmin.de.sh
+++ etc/extract/osm2garmin.de.sh
@@ -3,6 +3,7 @@ Map data (c) OpenStreetMap contributors, http://www.openstreetmap.org
 Extracts created by BBBike, http://BBBike.org
 mkgmap by http://www.mkgmap.org.uk
 
+
 Please read the OSM wiki how to install the maps on your GPS device:
 
   http://wiki.openstreetmap.org/wiki/OSM_Map_On_Garmin#Installing_the_map_onto_your_GPS
