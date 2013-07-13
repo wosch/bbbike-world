88e42f017f4972c2eef96eab26c4f2e0d79c44af
Wolfram Schneider <wosch@freebsd.org>
2013-07-13 19:18:38 +0000

    with CSV output

--- bin/tile-fixme.pl
+++ bin/tile-fixme.pl
@@ -5,6 +5,8 @@
 
 use IO::File;
 use Getopt::Long;
+use Data::Dumper;
+
 use lib 'world/bin';
 use lib '.';
 use TileSize;
@@ -13,6 +15,18 @@ use strict;
 use warnings;
 
 my $debug = 2;
+
+sub to_csv {
+    my $key = shift;
+    my $kb  = shift;
+
+    my ( $lng_sw, $lat_sw ) = split ",", $key;
+    my $lng_ne = $lng_sw + 1;
+    my $lat_ne = $lat_sw + 1;
+
+    return
+      $kb . "\t" . join( " ", ( $lng_sw, $lat_sw, $lng_ne, $lat_ne ) ) . "\n";
+}
 
 ######################################################################
 # GET /w/api.php?namespace=1&q=berlin HTTP/1.1
@@ -57,5 +71,10 @@ die "missinag format argument" . &usage if !$format;
 
 my $tile_pbf   = TileSize->new( 'database' => $database_pbf );
 my $tile_fixme = TileSize->new( 'database' => $database_fixme );
+
+#warn Dumper($tile_fixme->{_size});
+while ( my ( $key, $val ) = each %{ $tile_fixme->{_size} } ) {
+    print to_csv( $key, $val ) if $val > -1;
+}
 
 1;
