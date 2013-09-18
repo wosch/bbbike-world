19b3bf3aca1098ba1c80137fdcb5bdc40bcfc162
Wolfram Schneider <wosch@freebsd.org>
2013-09-18 17:55:43 +0000

    reformat subject line

--- bin/extract.pl
+++ bin/extract.pl
@@ -779,7 +779,7 @@ sub reorder_pbf {
         my $pbf_file = $obj->{'pbf_file'};
         my $format   = $obj->{'format'};
 
-        my $st   = stat($pbf_file) or die "stat $pbf_file: $!\n";
+        my $st = stat($pbf_file) or die "stat $pbf_file: $!\n";
         my $size = $st->size * $format{$format};
 
         $hash{$json_file} = $size;
@@ -1298,15 +1298,13 @@ qq[$obj->{"sw_lng"},$obj->{"sw_lat"} x $obj->{"ne_lng"},$obj->{"ne_lat"}],
 #http://www.BBBike.org - Your Cycle Route Planner
 #EOF
 
-        my @args = (
-            $obj->{'email'},
-            "BBBike extract: area is ready for download: '"
-              . $obj->{'city'}
-              . "', format="
-              . $obj->{'format'},
-            $message,
-            $option->{'bcc'}
-        );
+        my $subject =
+            "BBBike extract: area '"
+          . $obj->{'city'}
+          . "', format="
+          . $obj->{'format'}
+          . " is ready for download: '";
+        my @args = ( $obj->{'email'}, $subject, $message, $option->{'bcc'} );
 
         my $email_rest_enabled = $option->{"email_rest_enabled"};
         warn "email_rest_enabled: $email_rest_enabled\n" if $debug >= 2;
