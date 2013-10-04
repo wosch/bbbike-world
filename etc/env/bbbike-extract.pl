d95682caf2f4f8bd3a3048f59f2f8a1e8686f8bd
Wolfram Schneider <wosch@freebsd.org>
2013-10-04 12:05:00 +0200

    cleanup dot files hierachy

---
+++ etc/env/bbbike-extract.pl
@@ -0,0 +1,7 @@
+# example config file for bbbike/world/bin/extract.pl
+# 
+# put it in ~/.bbbike-extract 
+
+$option->{'homepage'} = 'http://download2.bbbike.org/osm/extract';
+#$option->{'bcc'} = 'nobody@gmail.com';
+
