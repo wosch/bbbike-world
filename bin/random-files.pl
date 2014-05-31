18ce75757e07a1ecccb1913279776a06864ece05
Wolfram Schneider <wosch@freebsd.org>
2014-05-31 18:54:57 +0200

    random-files.pl - sort arguments randomly

---
+++ bin/random-files.pl
@@ -0,0 +1,26 @@
+#!/usr/local/bin/perl
+# Copyright (c) 2011-2014 Wolfram Schneider, http://bbbike.org
+#
+# random-files.pl - sort arguments randomly
+#
+# ./random-files.pl ./t/*.t
+
+use strict;
+use warnings;
+
+sub out {
+   my @files = @_;
+
+   print join " ", @files, "\n";
+}
+
+sub random_sort {
+   my @files = @_;
+
+   my %m = map { $_ => rand() } @files;
+
+   return sort { $m{$a} <=> $m{$b} } keys %m;
+}
+
+&out ( $ENV{BBBIKE_RANDOM_FILES} ? &random_sort(@ARGV) : @ARGV);
+
