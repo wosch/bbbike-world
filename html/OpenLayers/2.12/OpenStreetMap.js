437e2b348af4d0ecbbeae7b69dc8ec7c4f6e6d28
Wolfram Schneider <wosch@freebsd.org>
2014-05-31 11:44:28 +0000

    updates from bbbike.org/mc for nokia here

--- html/OpenLayers/2.12/OpenStreetMap.js
+++ html/OpenLayers/2.12/OpenStreetMap.js
@@ -49,7 +49,7 @@ OpenLayers.Layer.OSM.Mapnik = OpenLayers.Class(OpenLayers.Layer.OSM, {
             "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
         ];
         options = OpenLayers.Util.extend({
-            numZoomLevels: 19,
+            numZoomLevels: 20,
             buffer: 0,
             transitionEffect: "resize"
         }, options);
@@ -544,52 +544,21 @@ OpenLayers.Layer.Yahoo = OpenLayers.Class(
     CLASS_NAME: "OpenLayers.Layer.Yahoo"
 });
 
-/* Nokia: openlayers/lib/OpenLayers/Layer/Nokia.js */
 
-/* Copyright (c) 2006-2013 by OpenLayers Contributors (see authors.txt for 
- * full list of contributors). Published under the 2-clause BSD license.
- * See license.txt in the OpenLayers distribution or repository for the
- * full text of the license. */
-
-/* Written by Wolfram Schneider, 2012-2013
- * Map Compare BBBike: http://mc.bbbike.org/mc
- *
- * based on XYZ.js and Bing.js example code
- */
-
-/**
- * @requires OpenLayers/Layer/XYZ.js
- */
-
-/** 
- * Class: OpenLayers.Layer.Nokia
- * Nokia layer using direct tile access as provided by Nokia Maps REST Services.
- * See http://developer.here.net/ for more
- * information. Note: Terms of Service compliant use requires the map to be
- * configured with an <OpenLayers.Control.Attribution> control and the
- * attribution placed on or near the map.
- * 
- * Inherits from:
- *  - <OpenLayers.Layer.XYZ>
- */
-
-/**
- * Example: map = new OpenLayers.Layer.Nokia("Nokia Map", { type: "normal.day", app_id: "abcdefgh"})
- */
 OpenLayers.Layer.Nokia = OpenLayers.Class(OpenLayers.Layer.XYZ, {
     // for reference: all supported layers
-    layers: ["normal.day", "terrain.day", "satellite.day", "hybrid.day", "normal.day.transit", "normal.day.grey"],
-    
+    layers: ["normal.day", "terrain.day", "satellite.day", "hybrid.day", "normal.day.transit", "newest/normal.day"],
+
     /**
      * APIProperty: name
      * {String} The layer name. Defaults to "OpenStreetMap" if the first
      * argument to the constructor is null or undefined.
      */
     name: "Nokia",
-    
+
     /**
      * Property: key
-     * {String} API key for Nokia maps, get your own key 
+     * {String} API key for Nokia maps, get your own key
      *     at http://developer.here.net/
      */
     app_id: null,
@@ -602,16 +571,16 @@ OpenLayers.Layer.Nokia = OpenLayers.Class(OpenLayers.Layer.XYZ, {
     attribution : '<span class="olNokiaAttribution">' +
          '<a target="_blank" href="http://maps.nokia.com/">Nokia.com</a>' +
          '</span>',
-         
-    
+
+
     /**
      * APIProperty: type
-     * {String} The layer identifier.  
+     * {String} The layer identifier.
      *     used.  Default is "normal.day".
      *
      */
     type: "normal.day",
-    
+
     /** APIProperty: tileOptions
      *  {Object} optional configuration options for <OpenLayers.Tile> instances
      *  created by this Layer. Default is
@@ -629,7 +598,7 @@ OpenLayers.Layer.Nokia = OpenLayers.Class(OpenLayers.Layer.XYZ, {
      * {Boolean}
      */
     sphericalMercator: true,
-    
+
     /**
      * Constructor: OpenLayers.Layer.Nokia
      *
@@ -642,22 +611,26 @@ OpenLayers.Layer.Nokia = OpenLayers.Class(OpenLayers.Layer.XYZ, {
      */
     initialize: function(name, options) {
         OpenLayers.Layer.XYZ.prototype.initialize.apply(this, arguments);
-        
+
         var type = (options.type || this.type);
         if (!checkLayerType(this.layers, type)) {
             throw "Unsupported Nokia map type: " + type;
         }
-        
+
         name = name || this.name;
-        var url = this.nokiaTileSeverUrl(type, { app_id: options.app_id, token: options.token});
+        var url = this.nokiaTileSeverUrl(type, {
+            app_id: options.app_id,
+            tile_id: options.tile_id,
+            token: options.token
+        });
 
         options = OpenLayers.Util.extend({
             numZoomLevels: 19
         }, options);
-        
+
         var newArgs = [name, url, options];
         OpenLayers.Layer.XYZ.prototype.initialize.apply(this, newArgs);
-        
+
         this.tileOptions = OpenLayers.Util.extend({
             crossOriginKeyword: 'anonymous'
         }, this.options && this.options.tileOptions);
@@ -667,32 +640,42 @@ OpenLayers.Layer.Nokia = OpenLayers.Class(OpenLayers.Layer.XYZ, {
     nokiaTileSeverUrl: function (type, opt) {
         var app_id = opt.app_id;
         var token = opt.token;
+        var tile_id = opt.tile_id;
         var servers = opt.servers;
+
+        if (!tile_id) // may change every 3 months (sic!)
+            tile_id = "f8c7b21875";
         
-        var url_prefix = "maps.nlp.nokia.com/maptile/2.1/maptile/14ca0f23ba";
-    
+        var urls = {
+            "normal.day": "base.maps.api.here.com/maptile/2.1/maptile/" + tile_id,
+            "terrain.day": "aerial.maps.api.here.com/maptile/2.1/maptile/" + tile_id,
+            "satellite.day": "aerial.maps.api.here.com/maptile/2.1/maptile/" + tile_id,
+            "hybrid.day": "aerial.maps.api.here.com/maptile/2.1/maptile/" + tile_id,
+            "normal.day.transit": "base.maps.api.here.com/maptile/2.1/maptile/" + tile_id,
+            "newest/normal.day": "traffic.maps.api.here.com/maptile/2.1/" + "traffictile"
+        };
+        
+        var url_prefix = urls[type];
+
         // traffic layer use a different API
         if (!servers || servers.length == 0) {
-            servers = (type == "newest/normal.day" ? [""] : ["1", "2", "3", "4"]);
+            servers = ["1", "2", "3", "4"];
         }
-        if (type == "newest/normal.day") { // traffic
-            url_prefix = "maps.nlp.nokia.com/maptile/2.1/traffictile";
-        }
-    
+
         var url_list = [];
         for (var i = 0; i < servers.length; i++) {
             url_list.push("http://" + (servers[i] ? servers[i] + "." : "") + url_prefix + "/" + type + "/${z}/${x}/${y}/256/png8?app_id=" + app_id + "&token=" + token + "&lg=ENG");
         }
-    
+
         return url_list;
     },
 
     /**
      * APIMethod: clone
-     * 
+     *
      * Parameters:
      * obj - {Object}
-     * 
+     *
      * Returns:
      * {<OpenLayers.Layer.Nokia>} An exact clone of this <OpenLayers.Layer.Nokia>
      */
@@ -705,7 +688,7 @@ OpenLayers.Layer.Nokia = OpenLayers.Class(OpenLayers.Layer.XYZ, {
         // copy/set any non-init, non-simple values here
         return obj;
     },
-    
+
     CLASS_NAME: "OpenLayers.Layer.Nokia"
 });
 
@@ -716,4 +699,3 @@ function checkLayerType(layers, type) {
    }
    return false;
 }
-
