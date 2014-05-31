3136a3e07e7bfa8f66bd638468845659c50cae8e
Wolfram Schneider <wosch@freebsd.org>
2014-05-31 17:05:05 +0000

    indent

--- bin/random-files.pl
+++ bin/random-files.pl
@@ -9,18 +9,18 @@ use strict;
 use warnings;
 
 sub out {
-   my @files = @_;
+    my @files = @_;
 
-   print join " ", @files;
+    print join " ", @files;
 }
 
 sub random_sort {
-   my @files = @_;
+    my @files = @_;
 
-   my %m = map { $_ => rand() } @files;
+    my %m = map { $_ => rand() } @files;
 
-   return sort { $m{$a} <=> $m{$b} } keys %m;
+    return sort { $m{$a} <=> $m{$b} } keys %m;
 }
 
-&out ( $ENV{BBBIKE_RANDOM_FILES} ? &random_sort(@ARGV) : @ARGV);
+&out( $ENV{BBBIKE_RANDOM_FILES} ? &random_sort(@ARGV) : @ARGV );
 
