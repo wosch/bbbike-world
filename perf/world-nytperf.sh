b3d4c0d74d7cc01b1f276c5eec55e3c65a534321
Wolfram Schneider <wosch@freebsd.org>
2014-07-18 17:05:41 -0400

    world-nytperf.sh - cgi/shell wrapper for bbbike @ world city perf testing

--- perf/world-nytperf.sh
+++ perf/world-nytperf.sh
@@ -2,13 +2,18 @@
 # Copyright (c) 2009-2014 Wolfram Schneider, http://bbbike.org
 #
 # world-nytperf.sh - cgi/shell wrapper for bbbike @ world city perf testing
+#
+# city=Cottbus ./world/perf/world-nytperf.sh
 
 export LANG="C"
 export PATH="/opt/local/bin:/bin:/usr/bin:/usr/local/bin"
 : ${city="Cottbus"}
+: ${QUERY_STRING=""}
 server=nytperf
+export SCRIPT_NAME=/cgi/world.cgi
 
 umask 002
+set -e
 
 cache_dir=/var/cache/bbbike/${server}/$city
 if [ "$server" = "localhost" ]; then
@@ -16,10 +21,12 @@ if [ "$server" = "localhost" ]; then
 fi
 mkdir -p $cache_dir
 
-export NYTPROF=trace=2:start=init:file=/tmp/nytprof.out
+export NYTPROF=trace=0:start=init:file=/tmp/nytprof.out
+#export NYTPROF=trace=2:start=init:file=/tmp/nytprof.out
 data="data-osm/$city"
 
-# run from bbbike top directory
+# run from world cgi directory
+cd world/cgi 
 time env TMPDIR=$cache_dir DATA_DIR="$data" BBBIKE_DATADIR="$data" \
-	perl -d:NYTProf cgi/bbbike.cgi 
+	perl -d:NYTProf ./bbbike.cgi 
 
