cd5053c718b9487d2f93f6e19a6d01ef9a007445
Wolfram Schneider <wosch@freebsd.org>
2014-09-13 17:03:15 +0000

    hide sidebar_dummy in fullscreen mode

--- html/maps3.js
+++ html/maps3.js
@@ -145,7 +145,7 @@ var state = {
     replay: false,
 
     // tags to hide in full screen mode
-    non_map_tags: ["copyright", "weather_forecast_html", "top_right", "other_cities", "footer", "routing", "route_table", "routelist", "link_list", "bbbike_graphic", "chart_div", "routes", "headlogo", "bottom", "language_switch", "headline", "sidebar"],
+    non_map_tags: ["copyright", "weather_forecast_html", "top_right", "other_cities", "footer", "routing", "route_table", "routelist", "link_list", "bbbike_graphic", "chart_div", "routes", "headlogo", "bottom", "language_switch", "headline", "sidebar", "sidebar_dummy"],
 
     // keep state of non map tags
     non_map_tags_val: {},
