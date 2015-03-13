fc85157b53aa77c6e2fb9bd9e5df2de90594b9ba
Wolfram Schneider <wosch@freebsd.org>
2015-03-13 08:18:01 -0400

    indent area.js

--- html/area.js
+++ html/area.js
@@ -6,31 +6,35 @@
 
 function plot_bbbike_areas(bbbike_db, offline) {
 
-var data = "";
-for(var i = 0; i < bbbike_db.length; i++) {
-    var obj = bbbike_db[i];
-    var c = obj[0];
-    
-    download_plot_polygon( {"color": "orange",
-     "sw_lng":  obj[1], "sw_lat": obj[2], "ne_lng": obj[3], "ne_lat": obj[4]});
-    
-    // footer links
-    data += '<a href="';
-    if (offline) {
-	data += '../' + c + '/';
-    } else {
-	data += '?city=' + c;
-    }
-	
-    data += '">' + c + '</a>\n';
-}
+    var data = "";
+    for (var i = 0; i < bbbike_db.length; i++) {
+        var obj = bbbike_db[i];
+        var c = obj[0];
 
-$("#more_cities_inner").html(data);
+        download_plot_polygon({
+            "color": "orange",
+            "sw_lng": obj[1],
+            "sw_lat": obj[2],
+            "ne_lng": obj[3],
+            "ne_lat": obj[4]
+        });
+
+        // footer links
+        data += '<a href="';
+        if (offline) {
+            data += '../' + c + '/';
+        } else {
+            data += '?city=' + c;
+        }
+
+        data += '">' + c + '</a>\n';
+    }
+
+    $("#more_cities_inner").html(data);
 }
 
 function set_map_height() {
     var padding = 10; // XXX: browser problems?
-    
     var height = $("body").height() - $('#bottom').height() - padding;
     if (height < 200) height = 200;
 
@@ -40,36 +44,35 @@ function set_map_height() {
 };
 
 function jump_to_city(bbbike_db, city) {
-  var obj;
-  for(var i = 0; i < bbbike_db.length; i++) {
-	obj = bbbike_db[i];
-	if (obj[0] == city) {
-	    center_city(obj[1], obj[2], obj[3], obj[4]);
-	    return;
-	}
-  }
-  debug("did not found city in db: " + city);
+    var obj;
+    for (var i = 0; i < bbbike_db.length; i++) {
+        obj = bbbike_db[i];
+        if (obj[0] == city) {
+            center_city(obj[1], obj[2], obj[3], obj[4]);
+            return;
+        }
+    }
+    debug("did not found city in db: " + city);
 }
 
-    function resizeOtherCities(toogle) {
-	var tag = document.getElementById("BBBikeGooglemap");
-	var tag_more_cities = document.getElementById("more_cities");
-    
-	if (!tag) return;
-	if (!tag_more_cities) return;
-    
-	if (!toogle) {
-	    // tag.style.height = "75%";
-	    // tag_more_cities.style.fontSize = "85%";
-	    tag_more_cities.style.display = "block";
-    
-	} else {
-	    tag_more_cities.style.display = "none";
-	    // tag.style.height = "90%";
-	}
-    
-	more_cities = toogle ? false : true;
-	// google.maps.event.trigger(map, 'resize');
-	set_map_height();
+function resizeOtherCities(toogle) {
+    var tag = document.getElementById("BBBikeGooglemap");
+    var tag_more_cities = document.getElementById("more_cities");
+
+    if (!tag) return;
+    if (!tag_more_cities) return;
+
+    if (!toogle) {
+        // tag.style.height = "75%";
+        // tag_more_cities.style.fontSize = "85%";
+        tag_more_cities.style.display = "block";
+
+    } else {
+        tag_more_cities.style.display = "none";
+        // tag.style.height = "90%";
     }
-    
+
+    more_cities = toogle ? false : true;
+    // google.maps.event.trigger(map, 'resize');
+    set_map_height();
+}
