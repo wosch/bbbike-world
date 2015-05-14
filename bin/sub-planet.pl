f355d99b5e555360d9e2c68cb9cede6c1628b3a1
Wolfram Schneider <wosch@freebsd.org>
2015-05-14 11:02:04 +0000

    refactor and support Getopt::Long

--- bin/sub-planet.pl
+++ bin/sub-planet.pl
@@ -4,6 +4,7 @@
 # create poly sub-planet files
 
 use IO::File;
+use Getopt::Long;
 
 use lib qw(world/lib ../lib);
 use Extract::Poly;
@@ -11,15 +12,21 @@ use Extract::Poly;
 use strict;
 use warnings;
 
-my $debug               = 1;
-my $sub_planet_dir      = '../osm/download/sub-planet';
-my $sub_planet_conf_dir = 'world/etc/sub-planet';
-my $planet_osm          = "../osm/download/planet-latest-nometa.osm.pbf";
+my $debug      = 1;
+my $prefix     = 'sub-planet';
+my $planet_osm = "../osm/download/planet-latest-nometa.osm.pbf";
 
-my $osmconvert_factor = 1.2;    # full Granularity
+sub usage () {
+    <<EOF;
+    
+usage: $0 [options]
 
-my $poly = new Extract::Poly( 'debug' => $debug );
-my @regions = $poly->list_subplanets;
+--debug=0..2            debug option
+--prefix=prefix         default: $prefix
+--planet=planet.osm.pbf default: $planet_osm
+
+EOF
+}
 
 sub store_data {
     my $file_real = shift;
@@ -40,31 +47,57 @@ sub store_data {
     rename( $file, $file_real ) or die "Rename $file -> $file_real: $!\n";
 }
 
-my @shell;
-foreach my $region (@regions) {
-    my $size    = $poly->subplanet_size($region);
-    my $size_mb = $poly->file_size_mb( $size * 1000 * $osmconvert_factor );
-    warn "region: $region: $size_mb MB\n" if $debug;
+sub regions {
+    my %args                = @_;
+    
+    my $sub_planet_dir      = $args{'sub_planet_dir'};
+    my $sub_planet_conf_dir = $args{'sub_planet_conf_dir'};
+    my $planet_osm          = $args{'planet_osm'};
 
-    my $obj = $poly->get_job_obj($region);
-    my ( $data, $counter ) = $poly->create_poly_data( 'job' => $obj );
+    my $osmconvert_factor = 1.2;    # full Granularity
 
-    my $file = "$sub_planet_conf_dir/$region.poly";
-    &store_data( $file, $data );
+    my $poly = new Extract::Poly( 'debug' => $debug );
+    my @regions = $poly->list_subplanets;
 
-    my @sh = (
-        "nice",           "-n15",
-        "time",           "osmconvert-wrapper",
-        "-o",             "$sub_planet_dir/$region.osm.pbf",
-        "-B=$file",       "--drop-author",
-        "--drop-version", "--out-pbf",
-        $planet_osm
-    );
-    push @shell, join " ", @sh;
+    my @shell;
+    foreach my $region (@regions) {
+        my $size    = $poly->subplanet_size($region);
+        my $size_mb = $poly->file_size_mb( $size * 1000 * $osmconvert_factor );
+        warn "region: $region: $size_mb MB\n" if $debug;
+
+        my $obj = $poly->get_job_obj($region);
+        my ( $data, $counter ) = $poly->create_poly_data( 'job' => $obj );
+
+        my $file = "$sub_planet_conf_dir/$region.poly";
+        &store_data( $file, $data );
+
+        my @sh = (
+            "nice",           "-n15",
+            "time",           "osmconvert-wrapper",
+            "-o",             "$sub_planet_dir/$region.osm.pbf",
+            "-B=$file",       "--drop-author",
+            "--drop-version", "--out-pbf",
+            $planet_osm
+        );
+        push @shell, join " ", @sh;
+    }
+
+    return @shell;
 }
 
-my $script = "$sub_planet_conf_dir/sub-planet.sh";
-warn "Now run ./world/bin/planet-sub\n" if $debug;
+#############################################################################
+# main
+#
+my $sub_planet_dir      = "../osm/download/$prefix";
+my $sub_planet_conf_dir = "world/etc/$prefix";
+
+my @shell = &regions(
+    'planet_osm'          => $planet_osm,
+    'sub_planet_dir'      => $sub_planet_dir,
+    'sub_planet_conf_dir' => $sub_planet_conf_dir
+);
+my $script = "$sub_planet_conf_dir/$prefix.sh";
+warn "Now run ./world/bin/$prefix\n" if $debug;
 store_data( $script, join "\n", @shell );
 
 __END__
