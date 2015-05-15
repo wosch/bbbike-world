adb614cb9abe6bb83c7eefd3a79deb65f5ac0e2c
Wolfram Schneider <wosch@freebsd.org>
2015-05-15 15:49:24 +0000

    first working version

--- bin/sub-planet-spool.pl
+++ bin/sub-planet-spool.pl
@@ -8,7 +8,7 @@
 use JSON;
 use Data::Dumper;
 
-use lib qw(world/lib);
+use lib qw(world/lib ../lib);
 use Extract::Utils;
 use Extract::Planet;
 use Extract::Poly;
@@ -16,10 +16,10 @@ use Extract::Poly;
 use strict;
 use warnings;
 
-my $debug   = 0;
-my $poly    = new Extract::Poly( 'debug' => $debug );
+my $debug          = 0;
+my $poly           = new Extract::Poly( 'debug' => $debug );
 my $planet         = new Extract::Planet( 'debug' => $debug );
-my @regions = $poly->list_subplanets(1);
+my @regions        = $poly->list_subplanets(1);
 my @regions_sorted = $poly->list_subplanets(1);
 
 sub get_polygon {
@@ -32,25 +32,26 @@ sub get_polygon {
 }
 
 sub get_smallest_planet {
-    my $obj = shift;
+    my $obj     = shift;
     my $regions = shift;
-    
+
     my @regions = @$regions;
-    
-    my ($data, $counter, $city_polygon) =  $poly->create_poly_data( 'job' => $obj );
+
+    my ( $data, $counter, $city_polygon ) =
+      $poly->create_poly_data( 'job' => $obj );
     warn Dumper($city_polygon) if $debug >= 1;
-    
+
     foreach my $outer (@regions_sorted) {
         my $inner = $planet->sub_polygon(
             'inner' => $city_polygon,
             'outer' => get_polygon($outer)
         );
-        
+
         if ($inner) {
             return $outer;
         }
     }
-    
+
     return "planet";
 }
 
@@ -58,18 +59,18 @@ sub get_smallest_planet {
 # main
 #
 
-binmode(\*STDOUT, ":utf8");
+binmode( \*STDOUT, ":utf8" );
 my $extract_utils = new Extract::Utils;
 die "No file given\n" if !@ARGV;
 
 foreach my $file (@ARGV) {
-my $obj = $extract_utils->parse_json_file($file);
-next if !exists $obj->{"coords"} or ref $obj->{"coords"} ne 'ARRAY';
+    my $obj = $extract_utils->parse_json_file($file);
+    next if !exists $obj->{"coords"} or ref $obj->{"coords"} ne 'ARRAY';
 
-warn Dumper($obj) if $debug >= 2;
+    warn Dumper($obj) if $debug >= 2;
 
-printf("%s\t%s\n", $obj->{"city"}, &get_smallest_planet($obj, \@regions_sorted));
+    printf( "%s\t%s\n",
+        $obj->{"city"}, &get_smallest_planet( $obj, \@regions_sorted ) );
 }
-
 
 __END__
