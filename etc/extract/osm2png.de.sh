be7554f50c74d2237aa08ae54fd6ad24bb36a04e
Wolfram Schneider <wosch@freebsd.org>
2015-09-06 16:07:36 +0000

    configure PNG format

---
+++ etc/extract/osm2png.de.sh
@@ -0,0 +1,21 @@
+cat << EOF
+Map data (c) OpenStreetMap contributors, http://www.openstreetmap.org
+Extracts created by BBBike, http://BBBike.org
+$BBBIKE_MAPERITIVE_VERSION by http://maperitive.net/
+
+
+Diese Garmin Karte wurde erzeugt am: $date
+Garmin Kartenstil: $maperitive_map_style
+GPS Rechteck Koordinaten (lng,lat): $BBBIKE_EXTRACT_COORDS
+Script URL: $BBBIKE_EXTRACT_URL
+Name des Gebietes: $city
+
+
+Spenden sind willkommen! Du kannst uns via PayPal, Flattr oder Bankueberweisung
+unterstuetzen: http://www.bbbike.org/community.de.html
+
+Danke, Wolfram Schneider
+
+--
+http://www.BBBike.org - Dein Fahrrad-Routenplaner
+EOF
