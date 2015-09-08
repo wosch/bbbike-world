bf5c3b7e183431567835804760546ac9e02cfd75
Wolfram Schneider <wosch@freebsd.org>
2015-09-08 20:08:03 +0000

    support SVG output

---
+++ etc/extract/osm2svg.en.sh
@@ -0,0 +1,21 @@
+cat << EOF
+Map data (c) OpenStreetMap contributors, http://www.openstreetmap.org
+Extracts created by BBBike, http://BBBike.org
+$BBBIKE_MAPERITIVE_VERSION by http://maperitive.net/
+
+
+This SVG map was created on: $date
+Maperitive map style: $maperitive_map_style
+GPS rectangle coordinates (lng,lat): $BBBIKE_EXTRACT_COORDS
+Script URL: $BBBIKE_EXTRACT_URL
+Name of area: $city
+
+We appreciate any feedback, suggestions and a donation! You can support us via
+PayPal, Flattr or bank wire transfer: http://www.BBBike.org/community.html
+
+thanks, Wolfram Schneider
+
+--
+http://www.BBBike.org - Your Cycle Route Planner
+EOF
+
