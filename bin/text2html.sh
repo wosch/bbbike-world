f2b0d8a94d3039e5ba36dd5112f13429ec995423
Wolfram Schneider <wosch@freebsd.org>
2015-09-19 21:06:30 +0000

    configure charset utf-8

--- bin/text2html.sh
+++ bin/text2html.sh
@@ -11,6 +11,7 @@ PATH=/usr/local/bin:/bin:/usr/bin; export PATH
   echo "<html><head><title>README</title></head><body><pre>"
   perl -npe 's,(https?://\S+),<a href="$1">$1</a>,gi'
   echo "</pre></body></html>"
-) | tidy -wrap -raw -i /dev/stdin 2>/dev/null
+) | tidy -wrap -raw -i /dev/stdin 2>/dev/null |
+    perl -npe 's,^  <meta name="generator" content="HTML Tidy.*\n,  <meta http-equiv="content-type" content="text/html; charset=utf-8">,'
 
 exit 0
