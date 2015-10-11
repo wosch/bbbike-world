6185ca6660858c3da84ae41119666ec8b659b28e
Wolfram Schneider <wosch@freebsd.org>
2015-10-11 17:18:44 +0000

    add translations files for scripts
    right now still in english

---
+++ etc/extract/osm2png.fr.sh
@@ -0,0 +1,25 @@
+cat << EOF
+Map data (c) OpenStreetMap contributors, https://www.openstreetmap.org
+Extracts created by BBBike, http://BBBike.org
+$BBBIKE_EXTRACT_MAPERITIVE_VERSION by http://maperitive.net/
+
+Please read the OSM wiki how to use PNG
+
+  https://wiki.openstreetmap.org/wiki/Export
+  https://en.wikipedia.org/wiki/Portable_Network_Graphics
+
+
+This PNG map was created on: $date
+Maperitive map style: $maperitive_map_style
+GPS rectangle coordinates (lng,lat): $BBBIKE_EXTRACT_COORDS
+Script URL: $BBBIKE_EXTRACT_URL
+Name of area: $city
+
+We appreciate any feedback, suggestions and a donation! You can support us via
+PayPal, Flattr or bank wire transfer: http://www.BBBike.org/community.html
+
+thanks, Wolfram Schneider
+
+--
+http://www.BBBike.org - Your Cycle Route Planner
+EOF
