c10e368fabadc9977fed5e67775a771b099f8ce0
Wolfram Schneider <wosch@freebsd.org>
2015-11-14 11:50:42 +0000

    refactor
    with help message at top

--- t/extract-formats.pl
+++ t/extract-formats.pl
@@ -1,6 +1,7 @@
-#!/usr/bin/perl
-
-use Getopt::Long;
+#!/usr/local/bin/perl
+# Copyright (c) Nov 2015 Wolfram Schneider, http://bbbike.org
+#
+# extract-formats.pl - test all extract formats
 
 use lib qw(world/lib);
 use Extract::Config;
@@ -18,7 +19,12 @@ my $sw_lat = -13.807;
 my $ne_lng = -71.732;
 my $ne_lat = -13.235;
 
+sub message {
+    print qq{# please run now: ./world/t/extract-formats.pl | xargs -P4 -n1 -0 /bin/sh -c >/dev/null \0};
+}
 
+sub generate_urls {
+    
 foreach my $key ( keys %$formats ) {
     print qq{curl -sSf "$server/cgi/extract.cgi}
       . qq{?sw_lng=$sw_lng&sw_lat=$sw_lat}
@@ -28,4 +34,12 @@ foreach my $key ( keys %$formats ) {
       . qq{&email=Nobody&as=1.933243109431466&pg=0.9964839602712444&coords=&oi=1}
       . qq{&city=test&lang=en&submit=extract&format=$key"\0};
 }
+}
 
+######################################################################
+# main
+#
+&message;
+&generate_urls;
+
+1;
