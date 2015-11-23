ad2e6e98c5a85c39b758c2969c749b20fb4235b9
Wolfram Schneider <wosch@freebsd.org>
2015-11-23 23:30:06 +0000

    MAPS.ME -> maps.me

--- lib/Extract/Config.pm
+++ lib/Extract/Config.pm
@@ -55,7 +55,7 @@ our $formats = {
     'csv.xz' => "csv 7z (xz)",
 
     'mapsforge-osm.zip' => "Mapsforge OSM",
-    'omim-osm.zip'      => "MAPS.ME OSM",
+    'omim-osm.zip'      => "maps.me OSM",
 
     'srtm-europe.osm.pbf'         => 'SRTM Europe PBF (25m)',
     'srtm-europe.osm.xz'          => 'SRTM Europe OSM XML 7z (25m)',
