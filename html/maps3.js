b957b792027d22a7d7ff81803100bcaf0c4679fa
Wolfram Schneider <wosch@freebsd.org>
2016-01-31 20:25:21 +0000

    (temporary) new keys for HERE maps

--- html/maps3.js
+++ html/maps3.js
@@ -1268,9 +1268,9 @@ function bbbike_maps_init(maptype, marker_list, lang, without_area, region, zoom
     };
 
     function nokia(a, z, name, servers) {
-        // [http://4.maptile.lbs.ovi.com/maptiler/v2/maptile/a2e328a0c5/normal.day/${z}/${x}/${y}/256/png8?app_id=SqE1xcSngCd3m4a1zEGb&token=r0sR1DzqDkS6sDnh902FWQ&lg=ENG"]
-        var app_id = "SqE1xcSngCd3m4a1zEGb";
-        var token = "r0sR1DzqDkS6sDnh902FWQ&lg";
+        // [http://4.maptile.lbs.ovi.com/maptiler/v2/maptile/a2e328a0c5/normal.day/${z}/${x}/${y}/256/png8?app_id=g7UuRR708Tsut4YSnDLy&token=fVzaDAdRK62zo3CuNcPtDg&lg=ENG"]
+        var app_id = "g7UuRR708Tsut4YSnDLy";
+        var token = "fVzaDAdRK62zo3CuNcPtDg&lg";
         var tile_style_version = "newest";
 
         if (!servers || servers.length == 0) {
