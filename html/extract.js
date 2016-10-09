6d955c333db6e65e35ec3b36fb68ec566dacfb71
Wolfram Schneider <wosch@freebsd.org>
2016-10-09 15:12:04 +0000

    add mapbox satellite
    will be used unstead bing until the key issues is resolved

--- html/extract.js
+++ html/extract.js
@@ -310,10 +310,18 @@ function init_map() {
         attribution: '<a href="https://www.openstreetmap.org/copyright">(&copy) OpenStreetMap contributors</a>, <a href="http://www.opencyclemap.org/">(&copy) OpenCycleMap</a>'
     }));
 
+    map.addLayer(new OpenLayers.Layer.OSM("Mapbox Satellite", "https://d.tiles.mapbox.com/v3/tmcw.map-j5fsp01s/${z}/${x}/${y}.png"), {
+        attribution: '<a href="https://www.mapbox.com/">(&copy) Mapbox</a>, <a href="https://www.mapbox.com/">(&copy) Mapbox</a>',
+        tileOptions: {
+            crossOriginKeyword: null
+        },
+        numZoomLevels: 20
+    });
+
+
     // Bing roads and Satellite/Hybrid
     // disabled due wrong billing
     // add_bing_maps(map);
-
     state.map = map;
     return map;
 }
