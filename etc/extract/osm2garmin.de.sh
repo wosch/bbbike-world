e0d6b5c306c93fe580a220cbab628df52a645e38
Wolfram Schneider <wosch@freebsd.org>
2016-10-15 13:06:19 +0000

    document OpenSeaMap.org in zip archive

--- etc/extract/osm2garmin.de.sh
+++ etc/extract/osm2garmin.de.sh
@@ -2,7 +2,7 @@ cat << EOF
 Map data (c) OpenStreetMap contributors, https://www.openstreetmap.org
 Extracts created by BBBike, http://extract.bbbike.org
 $BBBIKE_EXTRACT_GARMIN_VERSION by http://www.mkgmap.org.uk
-Map style (c) by OpenStreetMap.org, BBBike.org, OpenFietsMap.nl
+Map style (c) by OpenStreetMap.org, BBBike.org, OpenFietsMap.nl, OpenSeaMap.org
 
 
 Please read the OSM wiki how to install the maps on your GPS device:
