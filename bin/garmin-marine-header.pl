987efeeb8022fb5a4cb070a9ff87db6fba84f15c
Wolfram Schneider <wosch@freebsd.org>
2016-10-28 19:34:34 +0000

    first working version

---
+++ bin/garmin-marine-header.pl
@@ -0,0 +1,38 @@
+#!/usr/local/bin/perl
+# Copyright (c) 2014-2016 Wolfram Schneider, http://bbbike.org
+#
+# garmin-marine-header - set header for garmin marine devices
+#
+# 1. find the string "GARMIN TRE"
+# 2. move forward 56 characters
+# 3. replace to chracters, 0x04 and 0x17
+#
+# Note: the header may appears several times in the gmapsupp.img
+
+binmode \*STDIN,  ":bytes";
+binmode \*STDOUT, ":bytes";
+
+while (<>) {
+    # match the magic HEADER field
+    if (/GARMIN TRE....$/) {
+        print $_;
+
+	# move forward 51 bytes
+        for ( 1 .. 51 ) {
+            print getc( \*STDIN );
+        }
+
+	# set the new 2 bytes
+        printf( "%c%c", 4, 23 );
+
+	# skip the old 2 bytes
+        for ( 1 .. 2 ) { 
+		getc( \*STDIN ) 
+	}
+    }
+
+    else {
+        print;
+    }
+}
+
