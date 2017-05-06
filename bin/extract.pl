ff965c26a64ddd24bd4b167e4c682cdcf51b319c
Wolfram Schneider <wosch@freebsd.org>
2017-05-06 08:51:23 +0200

    refactor out function from extract.pl to Extract::Utils

--- bin/extract.pl
+++ bin/extract.pl
@@ -35,7 +35,7 @@ use LockFile::Simple;
 
 use lib qw(world/lib ../lib);
 use Extract::Config;
-use Extract::Utils qw(read_data);
+use Extract::Utils; 
 use Extract::Poly;
 use Extract::Planet;
 
@@ -173,82 +173,7 @@ $planet_osm =
 #
 #
 
-# timeout handler
-sub set_alarm {
-    my $time = shift;
-    my $message = shift || "";
 
-    $time = $alarm if !defined $time;
-
-    $SIG{ALRM} = sub {
-        my $pgid = getpgrp();
-
-        warn "Time out alarm $time\n";
-
-        # sends a hang-up signal to all processes in the current process group
-        # and kill running java processes
-        local $SIG{HUP} = "IGNORE";
-        kill "HUP", -$pgid;
-        sleep 0.5;
-
-        local $SIG{TERM} = "IGNORE";
-        kill "TERM", -$pgid;
-        sleep 0.5;
-
-        local $SIG{INT} = "IGNORE";
-        kill "INT", -$pgid;
-        sleep 0.5;
-
-        warn "Send a hang-up to all childs.\n";
-
-        #exit 1;
-    };
-
-    warn "set alarm time to: $time seconds $message\n" if $debug >= 1;
-    alarm($time);
-}
-
-sub get_loadavg {
-    my @loadavg = ( qx(uptime) =~ /([\.\d]+),?\s+([\.\d]+),?\s+([\.\d]+)/ );
-
-    warn "Current load average is: $loadavg[0]\n" if $debug >= 1;
-    return $loadavg[0];
-}
-
-# get a list of json config files from a directory
-sub get_jobs {
-    my $dir = shift;
-
-    my $d = IO::Dir->new($dir);
-    if ( !defined $d ) {
-        warn "error directory $dir: $!\n";
-        return ();
-    }
-
-    my @data;
-    while ( defined( $_ = $d->read ) ) {
-        next if !/\.json$/;
-        push @data, $_;
-    }
-    undef $d;
-
-    return @data;
-}
-
-# display output of a program to STDERR
-sub program_output {
-    my $program = shift;
-    my $fh = shift || \*STDERR;
-
-    if ( -e $program && -x $program ) {
-        open( OUT, "$program |" ) or die "$program: $!\n";
-        print $fh "$program:\n";
-        while (<OUT>) {
-            print $fh $_;
-        }
-        close OUT;
-    }
-}
 
 # returns 1 if we want to ignore a bot, otherwise 0
 sub ignore_bot {
@@ -288,20 +213,7 @@ sub ignore_bot {
     return 0;
 }
 
-# get a list of email addresses, and return a random list
-sub random_user {
-    my @list = @_;
 
-    my %hash = map { $_ => rand() } @list;
-
-    @list = sort { $hash{$a} <=> $hash{$b} } keys %hash;
-
-    if ( $debug >= 2 ) {
-        warn join " ", @list, "\n";
-    }
-
-    return @list;
-}
 
 sub get_sub_planet {
     my $obj = shift;
@@ -532,15 +444,6 @@ sub is_bot {
     return ( grep { $user_agent =~ /$_/ } @bots ) ? 1 : 0;
 }
 
-sub json_compat {
-    my $obj = shift;
-
-    # be backward compatible with old *.json files
-    if ( !( exists $obj->{'coords'} && ref $obj->{'coords'} eq 'ARRAY' ) ) {
-        $obj->{'coords'} = [];
-    }
-    return $obj;
-}
 
 # create a unique job id for each extract request
 sub get_job_id {
@@ -683,41 +586,6 @@ sub create_poly_files {
     return ( \@poly, \@json );
 }
 
-# refresh mod time of file, to keep files in cache
-sub touch_file {
-    my $file      = shift;
-    my $test_mode = shift;
-
-    my @system = ( "touch", $file );
-
-    warn "touch $file\n" if $debug >= 1;
-    @system = 'true' if $test_mode;
-
-    system(@system) == 0
-      or die "system @system failed: $?";
-}
-
-# store a blob of data in a file
-sub store_data {
-    my ( $file, $data ) = @_;
-
-    my $fh = new IO::File $file, "w" or die "open $file: $!\n";
-    binmode $fh, ":utf8";
-
-    print $fh $data;
-    $fh->close;
-}
-
-sub store_json {
-    my ( $file, $obj ) = @_;
-
-    my $file_tmp = "$file.tmp";
-    my $json     = new JSON;
-    my $data     = $json->pretty->encode($obj);
-
-    store_data( $file_tmp, $data );
-    rename( $file_tmp, $file ) or die "rename $file: $!\n";
-}
 
 # create a poly file which will be read by osmosis(1) to extract
 # an area from planet.osm
@@ -744,7 +612,7 @@ sub run_extracts {
       : &run_extracts_osmosis(@_);
 }
 
-#
+# Legacy
 # extract area(s) from planet.osm with osmosis tool
 #
 sub run_extracts_osmosis {
@@ -884,33 +752,6 @@ sub run_extracts_osmconvert {
     return ( \@data, \@fixme );
 }
 
-# compute SHA2 checksum for extract file
-sub checksum {
-    my $file = shift;
-    my $type = shift || 'sha256';
-
-    die "file $file does not exists\n" if !-f $file;
-
-    my @checksum_command = $type eq 'md5' ? qw/md5sum/ : qw/shasum -a 256/;
-
-    if ( my $pid = open( C, "-|" ) ) {
-    }
-
-    # child
-    else {
-        exec( @checksum_command, $file ) or die "Alert! Cannot fork: $!\n";
-    }
-
-    my $data;
-    while (<C>) {
-        my @a = split;
-        $data = shift @a;
-        last;
-    }
-    close C;
-
-    return $data;
-}
 
 # SMTP wrapper
 sub send_email_smtp {
@@ -1187,18 +1028,6 @@ sub convert_send_email {
     return $error_counter;
 }
 
-sub get_json {
-    my $json_file = shift;
-    my $json_text = read_data($json_file);
-    my $json      = new JSON;
-    my $obj       = $json->decode($json_text);
-    json_compat($obj);
-
-    warn "json: $json_file\n" if $debug >= 3;
-    warn "json: $json_text\n" if $debug >= 3;
-
-    return $obj;
-}
 
 # mkgmap.jar description limit of 50 bytes
 sub mkgmap_description {
@@ -1754,45 +1583,6 @@ sub fix_pbf {
               or die "system @system failed: $?";
         }
     }
-}
-
-# compare 2 files and return the modification diff time in seconds
-sub file_mtime_diff {
-    my $file1 = shift;
-    my $file2 = shift;
-
-    my $st1 = stat($file1) or die "stat $file1: $!\n";
-    my $st2 = stat($file2) or die "stat $file2: $!\n";
-
-    return $st1->mtime - $st2->mtime;
-}
-
-# file size in KB
-sub file_size {
-    my $file = shift;
-
-    my $st = stat($file) or die "stat $file: $!\n";
-
-    return $st->size;
-}
-
-# file size in x.y MB
-sub file_size_mb {
-    my $file = shift;
-
-    return kb_to_mb( file_size($file) );
-}
-
-# sacle file size in x.y MB
-sub kb_to_mb {
-    my $size = shift;
-
-    foreach my $scale ( 10, 100, 1000, 10_000 ) {
-        my $result = int( $scale * $size / 1024 / 1024 ) / $scale;
-        return $result if $result > 0;
-    }
-
-    return "0.0";
 }
 
 sub create_lock {
