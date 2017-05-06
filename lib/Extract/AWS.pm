90bf8dc093bae530ff98cdfd8250ee3bad61b4d4
Wolfram Schneider <wosch@freebsd.org>
2017-05-06 11:10:22 +0200

    new package Extract::AWS

--- lib/Extract/AWS.pm
+++ lib/Extract/AWS.pm
@@ -1,18 +1,9 @@
 #!/usr/local/bin/perl
 # Copyright (c) 2012-2017 Wolfram Schneider, https://bbbike.org
 #
-# extract config and libraries
+# extract AWS S3 functions
 
-package Extract::LockFile;
-
-use LockFile::Simple;
-use Data::Dumper;
-
-use Extract::Utils;
-
-require Exporter;
-use base qw/Exporter/;
-our @EXPORT = qw(create_lock remove_lock);
+package Extract::AWS;
 
 use strict;
 use warnings;
@@ -21,9 +12,10 @@ use warnings;
 # helper functions
 #
 
-our $debug = 0;
+our $debug  = 0;
+our $option = {};
 
-# Extract::LockFile::new->('debug' => 2)
+# Extract::LockFile::new->('debug' => 2, 'option' => $option)
 sub new {
     my $class = shift;
     my %args  = @_;
@@ -42,48 +34,53 @@ sub init {
     if ( defined $self->{'debug'} ) {
         $debug = $self->{'debug'};
     }
-}
-
-sub create_lock {
-    my %args     = @_;
-    my $lockfile = $args{'lockfile'};
-
-    warn "Try to create lockfile: $lockfile, value: $$\n" if $debug >= 1;
-
-    my $lockmgr = LockFile::Simple->make(
-        -hold      => 7200,
-        -autoclean => 1,
-        -max       => 5,
-        -stale     => 1,
-        -delay     => 1
-    );
-
-    if ( $lockmgr->trylock($lockfile) ) {
-        return $lockmgr;
-    }
-
-    # return undefined for failure
-    else {
-        warn "Cannot get lockfile, apparently in use: $lockfile\n"
-          if $debug >= 1;
-        return;
+    if ( defined $self->{'$option'} ) {
+        $option = $self->{'$option'};
     }
 }
 
-sub remove_lock {
+sub aws_s3_put {
+    my $self = shift;
     my %args = @_;
 
-    my $lockfile = $args{'lockfile'};
-    my $lockmgr  = $args{'lockmgr'};
+    my $file = $args{'file'};
 
-    my $pid = read_data("$lockfile.lock");    # xxx
-    chomp($pid);
+    if ( !$option->{"aws_s3_enabled"} ) {
+        warn "AWS S3 upload disabled\n" if $debug >= 3;
+        return;
+    }
 
-    warn "Remove lockfile: $lockfile, pid $pid\n" if $debug >= 1;
+    if ( !defined $file || !-e $file ) {
+        warn "No file '$file' given or exists for AWS S3 upload\n";
+        return;
+    }
 
-    $lockmgr->unlock($lockfile);
+    my $file_size = file_size_mb($file) . " MB";
+    warn "Upload $file with size $file_size to AWS S3\n" if $debug >= 1;
 
-    #unlink($lockfile) or die "unlink $lockfile: $!\n";
+    my $sep = "/";
+    my @system =
+      ( $option->{"aws_s3"}->{"put_command"}, aws_s3_path($file), $file );
+    warn join( " ", @system, "\n" ) if $debug >= 2;
+
+    system(@system) == 0
+      or die "system @system failed: $?";
+}
+
+sub aws_s3_path {
+    my $self = shift;
+    my $file = shift;
+
+    my $sep = "/";
+
+    my $aws_path =
+        $option->{"aws_s3"}->{"bucket"}
+      . $sep
+      . $option->{"aws_s3"}->{"path"}
+      . $sep
+      . basename($file);
+
+    return $aws_path;
 }
 
 1;
