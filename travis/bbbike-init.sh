449221d7ffdff675bd64f1451ede66eebd687f73
Wolfram Schneider <wosch@freebsd.org>
2017-06-14 13:02:48 +0000

    upgrade mono from wheezy to jessie

--- travis/bbbike-init.sh
+++ travis/bbbike-init.sh
@@ -49,7 +49,7 @@ init_apt_mono() {
 
     file="$sources_list_d/$mono_list"
     os=debian
-    codename=wheezy
+    codename=jessie
 
     if [ ! -e $file ]; then 
         sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
