c95e5717d0be449ee7a77da36cdb97f8ac36cb8a
Wolfram Schneider <wosch@freebsd.org>
2017-12-09 19:47:32 +0000

    larger shp files for pro service

--- html/extract.js
+++ html/extract.js
@@ -713,6 +713,7 @@ function extract_init_pro(opt) {
 
         config.max_size["default"] *= 1.7;
         config.max_size["osm.pbf"] *= 3;
+        config.max_size["shp.zip"] *= 4;
         config.max_skm *= 2;
 
         config.extract_pro = 1;
