38c9155b93539dcdca17bec12ec49f65d1e5f0c4
Wolfram Schneider <wosch@freebsd.org>
2018-06-23 14:36:20 +0000

    improve description

--- lib/Extract/CGI.pm
+++ lib/Extract/CGI.pm
@@ -144,7 +144,7 @@ sub header {
             {
                 -name => 'description',
                 -content =>
-'OpenStreetMap extracts from Planet.osm in OSM, PBF, Garmin, Osmand, mapsforge, Navit, SVG, GeoJSON, SQLite or Esri shapefile format (as rectangle or polygon).'
+'OpenStreetMap exports from Planet.osm in OSM, PBF, Garmin, Osmand, mapsforge, Navit, SVG, GeoJSON, SQLite or Esri shapefile format (as rectangle or polygon).'
             }
         )
     );
