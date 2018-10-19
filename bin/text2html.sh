f7f5b076ddec7a345bbb2c4bdc2b7c42ddeaa027
Wolfram Schneider <wosch@freebsd.org>
2018-10-19 10:06:51 +0200

    support file argument: text2html.sh file.txt

--- bin/text2html.sh
+++ bin/text2html.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-# Copyright (c) 2013 Wolfram Schneider, https://bbbike.org
+# Copyright (c) 2013-2018 Wolfram Schneider, https://bbbike.org
 #
 # text2html - convert a README.txt to README.html
 #
+# usage: ./text2html < README.txt > README.html
 
 #set -e
 PATH=/usr/local/bin:/bin:/usr/bin; export PATH
 
 (
   echo "<html><head><title>README</title></head><body><pre>"
-  perl -npe 's,(https?://\S+),<a href="$1">$1</a>,gi'
+  perl -npe 's,(https?://\S+),<a href="$1">$1</a>,gi' $1
   echo "</pre></body></html>"
 ) | tidy -wrap -raw -i /dev/stdin 2>/dev/null |
     perl -npe 's,^  <meta name="generator" content="HTML Tidy.*\n,  <meta http-equiv="content-type" content="text/html; charset=utf-8">,'
