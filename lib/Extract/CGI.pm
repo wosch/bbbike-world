ab6a4fd407c524c934f66ec9f45019bcb5bb7b8f
Wolfram Schneider <wosch@freebsd.org>
2019-07-28 08:50:33 +0000

    indent

--- lib/Extract/CGI.pm
+++ lib/Extract/CGI.pm
@@ -847,8 +847,8 @@ sub _check_input {
       $self->get_spool_dir() . "/" . $Extract::Config::spool->{"confirmed"};
 
     # limit per ip address, or for a given ip address
-    my $ip_limit = $option->{'scheduler'}->{'ip_limit_address'}->{$ip_address} // 
-		$option->{'scheduler'}->{'ip_limit'};
+    my $ip_limit = $option->{'scheduler'}->{'ip_limit_address'}->{$ip_address}
+      // $option->{'scheduler'}->{'ip_limit'};
 
     # a limit per user, see $cgi/extract.cgi::option
     my $email_limit = $option->{'scheduler'}->{'user_limit_email'}->{$email}
