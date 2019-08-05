9457a92766e5fe5dc2a2c13f8358fd3b3bb6bbd1
Wolfram Schneider <wosch@freebsd.org>
2019-08-05 01:57:53 +0000

    africa is bigger now than asia

--- etc/sub-planet/sub-planet.sh
+++ etc/sub-planet/sub-planet.sh
@@ -7,7 +7,7 @@ nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/europe-northwest.
 nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/europe-south.osm.pbf -B=world/etc/sub-planet/europe-south.poly --drop-author --drop-version --out-pbf ../osm/download/planet-latest-nometa.osm.pbf
 nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/europe-east.osm.pbf -B=world/etc/sub-planet/europe-east.poly --drop-author --drop-version --out-pbf ../osm/download/planet-latest-nometa.osm.pbf
 nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/north-america-east.osm.pbf -B=world/etc/sub-planet/north-america-east.poly --drop-author --drop-version --out-pbf ../osm/download/planet-latest-nometa.osm.pbf
-nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/asia-south.osm.pbf -B=world/etc/sub-planet/asia-south.poly --drop-author --drop-version --out-pbf ../osm/download/planet-latest-nometa.osm.pbf
 nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/africa.osm.pbf -B=world/etc/sub-planet/africa.poly --drop-author --drop-version --out-pbf ../osm/download/planet-latest-nometa.osm.pbf
+nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/asia-south.osm.pbf -B=world/etc/sub-planet/asia-south.poly --drop-author --drop-version --out-pbf ../osm/download/planet-latest-nometa.osm.pbf
 nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/north-america-west.osm.pbf -B=world/etc/sub-planet/north-america-west.poly --drop-author --drop-version --out-pbf ../osm/download/planet-latest-nometa.osm.pbf
 nice -n7 time osmconvert-wrapper -o ../osm/download/sub-planet/south-america.osm.pbf -B=world/etc/sub-planet/south-america.poly --drop-author --drop-version --out-pbf ../osm/download/planet-latest-nometa.osm.pbf
