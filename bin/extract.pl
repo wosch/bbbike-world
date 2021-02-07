2e3c50d4b2b5bac14f544ea9e413f41555ea2d03
Wolfram Schneider <wosch@freebsd.org>
2021-02-07 11:55:53 +0000

    fix newlines debug output

--- bin/extract.pl
+++ bin/extract.pl
@@ -926,7 +926,7 @@ sub reorder_pbf {
     my @json = sort { $hash{$a} <=> $hash{$b} } keys %hash;
     if ( $debug >= 2 ) {
         warn "Number of json files: " . scalar(@$json) . "\n";
-        warn join "\n", ( map { "$_ $hash{$_}" } @$json ), "\n";
+        warn join "\n", ( map { "$_ $hash{$_}" } @$json ), "";
     }
 
     return @json;
