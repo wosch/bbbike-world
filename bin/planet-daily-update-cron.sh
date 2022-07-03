f2db6a698f59086af20f59f20a3aed4fad4abcf0
Wolfram Schneider <wosch@freebsd.org>
2022-07-03 10:59:28 +0000

    improve log output handling

--- bin/planet-daily-update-cron.sh
+++ bin/planet-daily-update-cron.sh
@@ -4,17 +4,19 @@
 # planet-daily-update-cron - wrapper for planet-daily-update called by a cron job
 #
 
-PATH=/usr/local/bin:/bin:/usr/bin; export PATH 
+PATH="/usr/local/bin:/bin:/usr/bin"; export PATH 
 set -e
 
 cd $HOME/projects/bbbike
+logfile="tmp/log.planet-daily-update"
+touch $logfile
 
-if time nice -n 6 make planet-daily-update sub-planet-daily > tmp/log.planet-daily-update 2>&1; then
+if time nice -n 6 make planet-daily-update sub-planet-daily > $logfile 2>&1; then
   exit 0
 else
   echo "planet update failed: $?"
   echo ""
-  cat tmp/log.planet-update
+  cat $logfile
   exit 1
 fi
 
