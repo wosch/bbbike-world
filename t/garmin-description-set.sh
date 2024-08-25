63def1d8e263752b60541cf5a7481f0d76623be0
Wolfram Schneider <wosch@FreeBSD.org>
2024-08-25 14:09:33 +0000

    use temp files for in/out

--- t/garmin-description-set.sh
+++ t/garmin-description-set.sh
@@ -13,8 +13,10 @@ tmpdir=$(mktemp -d)
 dir=$(dirname $0)
 
 file=$dir/data-osm/Oderberg.osm.pbf
+file2=$tmpdir/$(basename $file)
 
-cp $file $tmpdir
-$dir/../bin/osm2garmin $file osm "$garmin_description osm/UTF-8 BBBike.org 2024-08-24"
+cp $file $file2
+
+$dir/../bin/osm2garmin "$file2" osm "$garmin_description osm/UTF-8 BBBike.org 2024-08-24"
 
 #EOF
